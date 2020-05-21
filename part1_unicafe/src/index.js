da126a3a67a1a81e4669a09aa260241b0d13572e
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-05-21 16:16:14 +0100

    added counter to each feedback unicafe

--- part1_unicafe/src/index.js
+++ part1_unicafe/src/index.js
@@ -1,7 +1,52 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({onClick, feedback}) => {
+  return (
+    <div>
+      <button onClick={onClick}>{feedback}</button>
+    </div>
+  )
+}
 
+const Results = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addPositiveFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addNegativeFeedback = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h3>Hello, welcome to Unicafe Feedback. Select one of the options below</h3>
+      <Button onClick={addPositiveFeedback} feedback="Good"/>
+      <Button onClick={addNeutralFeedback} feedback="Neutral"/>
+      <Button onClick={addNegativeFeedback} feedback="Bad"/>
+      <Results good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
 ReactDOM.render(
   <App />,
   document.getElementById('root')
