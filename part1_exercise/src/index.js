7f97be3069646515c51688e3d6222c32cefec497
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-05-22 16:16:11 +0100

    more exercises following on the part1_exercise

--- part1_exercise/src/index.js
+++ part1_exercise/src/index.js
@@ -1,62 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
+const Header = ({courseName}) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{courseName}</h1>
     </>
   )
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
+      <ul>
+        {parts.map((part) => (
+          <li key={part.id}>{part.name}</li>
+        )
+      )}
+      </ul>
+      <Total parts={parts}/>
     </>
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
+  const numberOfExercises = []
+  parts.map(part => numberOfExercises.push(part.exercises))
   return (
     <>
-      <p>Number of exercises {props.number} </p>
+      <p>Number of exercises {numberOfExercises.reduce((total, num) => total + num)} </p>
     </>
   )
 }
 
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header courseName={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id:1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   } 
 
-
-  console.log(course.parts)
   return (
-    <div>
-      <Header course={course.name}/>
-      <Content part={course.parts[0]}/>
-      <Content part={course.parts[1]}/>
-      <Content part={course.parts[2]}/>
-      <Total number={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
-    </div>
+    <Course course={course} />
   )
 }
 ReactDOM.render(
