98d17d02ac32cb4d8bccdf193602cf85d7546584
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-05-24 13:20:36 +0100

    moved all dependencies out into seperate components

--- part2_b/src/index.js
+++ part2_b/src/index.js
@@ -1,28 +1,38 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    {name: 'Arto Hellas', number: '999', id: 0}
-  ])
 
+const Persons = ({persons, setPersons}) => {
+  return (
+    <div>
+      {persons.map((person) => <li key={person.id}>{person.name}: {person.number}</li>)}
+    </div>
+  )
+}
+const Filter = ({persons}) => {
+  const [ filterResults, setFilterResults ] = useState([])
+
+  const handleFilterResults = (event) => {
+    console.log(event.target.value)
+    setFilterResults(persons.filter(function(person) {
+      return person.name.includes(event.target.value) 
+    }))
+    console.log(filterResults)
+  }
+
+  return (
+    <div>
+      Search by name:
+      <input type="text" onChange={handleFilterResults}/>
+      {filterResults.map((result) => <li key={result.id}>{result.name}: {result.number}</li>)}
+    </div>
+  )
+}
+
+const NewPersonForm = ({persons, setPersons}) => {
   const [ newNumber, setNewNumber ] = useState('')
 
   const [ newName, setNewName ] = useState('')
-
-  const [ filterResults, setFilterResults ] = useState([])
-
-  const checkPersonExists = () => {
-    let boolean 
-    persons.filter(function(person) {
-      if(person.name === newName) {
-        boolean = false
-      } else {
-        boolean = true
-      }
-    })
-    return boolean
-  }
 
   const addName = (event) => {
     event.preventDefault()
@@ -51,40 +61,48 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleFilterResults = (event) => {
-    console.log(event.target.value)
-    setFilterResults(persons.filter(function(person) {
-      return person.name.includes(event.target.value) 
-    }))
-    console.log(filterResults)
+  const checkPersonExists = () => {
+    let boolean 
+    persons.filter(function(person) {
+      if(person.name === newName) {
+        boolean = false
+      } else {
+        boolean = true
+      }
+    })
+    return boolean
   }
+
+  return (
+    <form onSubmit={addName}>
+      <div>
+        Name: <input value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        Number: <input value={newNumber} onChange={handleNumberChange}/>
+      </div>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    {name: 'Arto Hellas', number: '999', id: 0}
+  ])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Search by name:
-        <input type="text" onChange={handleFilterResults}/>
-        {filterResults.map((result) => <li key={result.id}>{result.name}: {result.number}</li>)}
-      </div>
-      <form onSubmit={addName}>
-        <div>
-          Name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          Number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form>
+      <Filter persons={persons}/>
+      <NewPersonForm persons={persons} setPersons={setPersons}/>
       <h2>Numbers</h2>
-      {persons.map((person) => <li key={person.id}>{person.name}: {person.number}</li>)}
+      <Persons persons={persons} setPersons={setPersons}/>
     </div>
   )
-
 }
-
 
 ReactDOM.render(
   <App />,
