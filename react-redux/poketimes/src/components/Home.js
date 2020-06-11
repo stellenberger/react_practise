dd220a3dd2868622ef4690d56d6a1691463655f6
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-11 14:38:15 +0100

    added state to posts to find the post using an axios request, and then output it using a ternery operator before rendering

--- react-redux/poketimes/src/components/Home.js
+++ react-redux/poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   state = {
@@ -24,7 +25,9 @@ class Home extends React.Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id} >
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
--- react-redux/poketimes/src/components/Post.js
+++ react-redux/poketimes/src/components/Post.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 class Post extends React.Component {
   state = {
-    id: null
+    post: null
   }
 
   componentDidMount() {
     let id = this.props.match.params.post_id
-    this.setState({
-      id
-    })
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then(response => {
+        this.setState({
+          post: response.data
+        })
+      })
   }
 
   render() {
+
+    const post = this.state.post ? (
+      <div className="post">
+        <h3 className="center">{this.state.post.title}</h3>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    )
     return (
       <div className="container">
-        <h4>{this.state.id}</h4>
+        {post}
       </div>
     )
   }
