084ef1a8963a6fe210e894658a16a0da67b1c96e
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-11 11:23:26 +0100

    using lifecycle hooks such as componentDidMount and componentDidUpdate and included a delete function for ninjas using arrow syntax

--- react-redux/my-app/src/App.js
+++ react-redux/my-app/src/App.js
@@ -20,12 +20,27 @@ class App extends React.Component {
     })
   }
 
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id 
+    })
+    this.setState({ninjas: ninjas})
+  }
+
+  componentDidMount(){
+    console.log('component mounted')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated')
+    console.log(prevProps, prevState);
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome!</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
         <AddNinja addNinja={this.addNinja} />
       </div>
     );
--- react-redux/my-app/src/components/Ninjas.js
+++ react-redux/my-app/src/components/Ninjas.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 // this is called a UI component as it doesnt have state, and its 
 // data is passed down as props.
-const Ninjas = (props) => {
-  const { ninjas } = props
+const Ninjas = ({ninjas, deleteNinja}) => {
   const ninjaList = ninjas.map(ninja => {
     return ninja.age > 20 ? (
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
           <div>Age: {ninja.age}</div>
           <div>Belt: {ninja.belt}</div>
+          <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
         </div>
     ) : null;
   })
