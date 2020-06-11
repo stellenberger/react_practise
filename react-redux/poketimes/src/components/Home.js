8cd4eb1c0e9a87cd4dda18039d768a66640360bf
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-11 14:08:41 +0100

    using the lifecycle hook componentDidMount to get json data from an external server using axios

--- react-redux/poketimes/src/components/Home.js
+++ react-redux/poketimes/src/components/Home.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import axios from 'axios'
 
-const Home = () => {
-  return(
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga adipisci laboriosam nemo ipsa qui, similique reprehenderit, expedita quidem culpa quos quaerat dolores, voluptate a aliquid voluptatem nam repellat consequuntur dignissimos.</p>
-    </div>
-  )
+class Home extends React.Component {
+  state = {
+    posts: [
+
+    ]
+  }
+
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        this.setState({
+          posts: response.data.slice(0, 10)
+        })
+      })
+  }
+  
+  render() {
+    const { posts } = this.state
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+      return(
+        <div className="container">
+          <h4 className="center">Home</h4>
+          {postList}
+        </div>
+      )
+  }
 }
 
 export default Home
