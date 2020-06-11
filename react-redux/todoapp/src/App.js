20d5a862f923ff71283a9d197b3b4a0ad342676f
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-11 12:23:33 +0100

    simple todo app using materialize css and react components

---
+++ react-redux/todoapp/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        content: 'buy some milk'
+      },
+      {
+        id: 2,
+        content: 'play mario cart'
+      }
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({ todos})
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
