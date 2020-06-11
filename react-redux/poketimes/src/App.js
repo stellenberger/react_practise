c13703a368d3420b98797f3a04b557214e772c86
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-11 12:40:01 +0100

    setting up react router using npm install react-router-dom and importing it into the app component

--- react-redux/poketimes/src/App.js
+++ react-redux/poketimes/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 
 class App extends React.Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Route path='/' component={Home}/>
       </div>
+      </BrowserRouter>
     ); 
   }
 }
