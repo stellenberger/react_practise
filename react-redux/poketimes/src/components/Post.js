135c51a7bbbbe4e2a6d6c51f5e46cb945626b6fd
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-12 18:44:56 +0100

    using dispatch to delete posts through the rootReducer

--- react-redux/poketimes/src/components/Post.js
+++ react-redux/poketimes/src/components/Post.js
@@ -2,11 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux' 
 
 class Post extends React.Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id)
+    this.props.history.push('/')
+  }
+
   render() {
+    console.log(this.props)
     const post = this.props.post ? (
       <div className="post">
         <h3 className="center">{this.props.post.title}</h3>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete this post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -26,4 +37,13 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) }
+  }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
