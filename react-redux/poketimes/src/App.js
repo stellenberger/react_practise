c5e516b44a16e295649abb7d928a6001c2bc3caa
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-12 16:35:22 +0100

    using switch to ensure only one component is loaded at a time

--- react-redux/poketimes/src/App.js
+++ react-redux/poketimes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -11,13 +11,15 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/contact' component={Contact}/>
-        <Route path='/:post_id' component={Post}/>
-      </div>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/contact' component={Contact}/>
+            <Route path='/:post_id' component={Post}/>
+          </Switch>
+        </div>
       </BrowserRouter>
     ); 
   }
