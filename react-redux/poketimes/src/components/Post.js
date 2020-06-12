774c6b1b000b7828b950e04ad41893f7797da534
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-12 17:41:29 +0100

    using the mapStateToProps to grab the params of the post ID and search the redux central store for the matching post

--- react-redux/poketimes/src/components/Post.js
+++ react-redux/poketimes/src/components/Post.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux' 
 
 class Post extends React.Component {
-  state = {
-    post: null
-  }
-
-  componentDidMount() {
-    let id = this.props.match.params.post_id
-    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-      .then(response => {
-        this.setState({
-          post: response.data
-        })
-      })
-  }
-
   render() {
-
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h3 className="center">{this.state.post.title}</h3>
-        <p>{this.state.post.body}</p>
+        <h3 className="center">{this.props.post.title}</h3>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -34,4 +19,11 @@ class Post extends React.Component {
   }
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => post.id == id)
+  }
+}
+
+export default connect(mapStateToProps)(Post)
