055ba4ba271282635b8e13ec6719bbe10b6beffe
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-12 17:22:47 +0100

    removed the json placeholder api along with axios from the homepage and instead grabbing data from the redux central store using the higher order connect function

--- react-redux/poketimes/src/reducers/rootReducer.js
+++ react-redux/poketimes/src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: 1, title: 'This is some dummy data and I LOVE PEPSI', body: 'lorem ipsum, dolor oaisjdoai aodaoisj o aoids joa do ad'},
+    {id: 2, title: 'This is some more dummy data and i dont know what I am doing with redux, but I am learning slowly!', body: 'lorem ipsum, dolor oaisjdoai aodaoisj o aoids joa do ad'},
+    {id: 3, title: 'WoOOOOOO take me on holiday, I need the sun!', body: 'lorem ipsum, dolor oaisjdoai aodaoisj o aoids joa do ad'}
+  ]
 }
 
 const rootReducer = (state = initState, action) => {
