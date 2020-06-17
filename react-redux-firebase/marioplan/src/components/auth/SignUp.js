d405667ccfd2493c18e20b9f1208a886f38552f4
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-17 17:05:29 +0100

    sign up functionality added inclusive of error messages on sign up

--- react-redux-firebase/marioplan/src/components/auth/SignUp.js
+++ react-redux-firebase/marioplan/src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
 class SignUp extends Component {
   state = {
     email: '',
@@ -17,7 +18,7 @@ class SignUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+    this.props.signUp(this.state)
   }
 
   render() {
@@ -45,7 +46,10 @@ class SignUp extends Component {
             <input type="text" id="lastName" onChange={this.handleChange}/>
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+          <div className="red-text center">
+            { authError ? <p>{ authError }</p> : null}
           </div>
         </form>
       </div>
@@ -55,8 +59,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
-export default connect(mapStateToProps)(SignUp)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
