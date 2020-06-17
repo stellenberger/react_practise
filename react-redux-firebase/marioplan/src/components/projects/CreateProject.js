66a959294f4c5db68fe54167f1c9599ad7d2425b
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-17 16:39:59 +0100

    using the current authentication status, we are using redirect to ensure which pages are accessible to both logged in and logged out users

--- react-redux-firebase/marioplan/src/components/projects/CreateProject.js
+++ react-redux-firebase/marioplan/src/components/projects/CreateProject.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createProject } from '../../store/actions/projectActions'
-
+import { Redirect } from 'react-router-dom'
 class CreateProject extends Component {
   state = {
     title: '',
@@ -20,6 +20,9 @@ class CreateProject extends Component {
   }
 
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />
+    
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -46,4 +49,10 @@ const mapDispatchToProps = (dispatch) => {
     createProject: (project) => dispatch(createProject(project))
   }
 }
-export default connect(null, mapDispatchToProps)(CreateProject)
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
