8bd6053abbe4c4138886c27ca4a1041a560f470a
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-17 16:13:12 +0100

    signed out functionality added

--- react-redux-firebase/marioplan/src/store/actions/authActions.js
+++ react-redux-firebase/marioplan/src/store/actions/authActions.js
@@ -10,6 +10,15 @@ export const signIn = (credentials) => {
     }).catch((err) => {
       dispatch({ type: 'LOGIN_ERROR', err })
     });
-    
+  }
+}
+
+export const signOut = () => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signOut().then(() => {
+      dispatch({ type: 'SIGNOUT_SUCCESS' });
+    })
   }
 }
