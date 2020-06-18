31514113011568af32cb16f0a2715099fadcedb9
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-18 17:09:06 +0100

    project summary now shows dynamic data of poster

--- react-redux-firebase/marioplan/src/store/actions/projectActions.js
+++ react-redux-firebase/marioplan/src/store/actions/projectActions.js
@@ -1,11 +1,13 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid
     const firestore = getFirestore();
     firestore.collection('projects').add({
       ...project, 
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorID: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorID: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT', project: project});
