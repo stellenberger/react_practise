bc306d450118469cae01673b61133af162eb08e2
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-18 17:52:57 +0100

    added date to project summary

--- react-redux-firebase/marioplan/src/components/projects/ProjectSummary.js
+++ react-redux-firebase/marioplan/src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({ project }) => {
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
           <p>Posted by the {project.authorFirstName} {project.authorLastName}</p>
-        <p className="grey-text">3rd September, 2am</p>
+        <p className="grey-text">{project.createdAt.toDate().toDateString()}</p>
       </div>
     </div>
   )
