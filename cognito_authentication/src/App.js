b9d3d7e4bd7ece2576e1cfe03137dfc33128d355
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-27 18:07:13 +0100

    setting up the form

--- cognito_authentication/src/App.js
+++ cognito_authentication/src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { CognitoUserPool } from 'amazon-cognito-identity-js'
 // downloaded using npm i amazon-cognito-identity-js
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const poolData = {
-    UserPoolId: '',
+    UserPoolId: process.env.REACT_APP_USER_ID,
     ClientId: process.env.REACT_APP_CLIENT_ID
   }
 
-  console.log('hello', process.env.REACT_APP_CLIENT_ID)
+  const UserPool = new CognitoUserPool(poolData)
   
+  const onSubmit = event => {
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      Hello World
+      <form onSubmit={onSubmit}>
+        <input 
+          value={email}
+          onChange={event => setEmail(event.target.value)}  
+        />
+
+        <input 
+          value={password}
+          onChange={event => setPassword(event.target.value)}  
+        />
+
+        <input type="submit"/>
+      </form>
     </div>
   );
 }
