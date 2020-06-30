f37ab058ab2c0731d1f381ca3c0cc009598c791e
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-30 14:26:36 +0100

    mern stack initial commit

--- mongodb_practise/src/App.js
+++ mongodb_practise/src/App.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 
 function App() {
+
+
+
   const [secretName, setSecretName] = useState('');
 
-  
-  const onSubmit = (e) => {
+  const handleOnChange = (e) => {
+    setSecretName(e.target.value)
+    console.log(secretName)
+  }
+
+  const handleOnSubmit = (e) => {
     e.preventDefault()
     console.log('hello from onSubmit')
+    
   }
   
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleOnSubmit}>
         <label>Input your secret name!</label><br/>
-        <input type="text"/><br/>
+        <input type="text" onChange={handleOnChange} value={secretName}/><br/>
         <button type='submit'>Submit this form</button>
       </form>
     </div>
